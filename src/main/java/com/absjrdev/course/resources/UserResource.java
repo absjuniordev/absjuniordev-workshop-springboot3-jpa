0a5b325797f062d2b12860075422ad4291a017ad
Arnaldo Junior <abs.junnior@hotmail.com>
2024-12-05 21:16:05 -0300

    User entity and resource

---
+++ src/main/java/com/absjrdev/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.absjrdev.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.absjrdev.course.entities.User;
+
+@RestController
+@RequestMapping("/user")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(null, "Fau", "fau@gmail.com", "99999", "1234");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
