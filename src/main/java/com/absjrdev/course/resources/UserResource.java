f0bbb564eb9d11a4b7a7ea9da4e547ec75ff5128
Arnaldo Junior <abs.junnior@hotmail.com>
2024-12-10 14:43:10 -0300

    User insert

--- src/main/java/com/absjrdev/course/resources/UserResource.java
+++ src/main/java/com/absjrdev/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.absjrdev.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.absjrdev.course.entities.User;
 import com.absjrdev.course.services.UserService;
@@ -15,7 +19,7 @@ import com.absjrdev.course.services.UserService;
 @RestController
 @RequestMapping("/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService userService;
 
@@ -24,11 +28,19 @@ public class UserResource {
 		List<User> list = userService.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User user = userService.findById(id);
 		return ResponseEntity.ok().body(user);
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = userService.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 
 }
