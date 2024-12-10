62d222736533062d983337004b4633863155012a
Arnaldo Junior <abs.junnior@hotmail.com>
2024-12-09 21:47:26 -0300

    Payment, one-to-one association

--- src/main/java/com/absjrdev/course/entities/Order.java
+++ src/main/java/com/absjrdev/course/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.absjrdev.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -28,17 +30,19 @@ public class Order implements Serializable {
 	private Long id;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	private Integer orderStatus;	
+	private Integer orderStatus;
 
 	// Muitos pedidos para um cliente
 	@ManyToOne
 	// chave estranjeira
 	@JoinColumn(name = "client_id")
 	private User client;
-	
-	@OneToMany(mappedBy = "id.order" )
+
+	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 		// TODO Auto-generated constructor stub
@@ -75,18 +79,26 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-	
+
 	public OrderStatus getOrderStatus() {
-		return OrderStatus.valueOf(orderStatus) ;
+		return OrderStatus.valueOf(orderStatus);
 	}
 
 	public void setOrderStatus(OrderStatus orderStatus) {
 		if (orderStatus != null) {
-			
+
 			this.orderStatus = orderStatus.getCode();
 		}
 	}
-	
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItems() {
 		return items;
 	}
