25ce8b92314b0afd3067d17bef2f1b5962b4e32f
Arnaldo Junior <abs.junnior@hotmail.com>
2024-12-16 12:27:50 -0300

    Implementation document swagger

---
+++ src/main/java/com/absjrdev/course/resources/doc/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.absjrdev.course.resources.doc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+	 @Bean
+	    public OpenAPI customOpenAPI() {
+	        return new OpenAPI()
+	                .info(new Info()
+	                    .title("ABS")
+	                    .version("1.0.0")
+	                    .description("E-comerce")
+	                    .contact(new io.swagger.v3.oas.models.info.Contact()
+	                        .name("Arnaldo Junior")
+	                        .url("https://github.com/absjuniordev")
+	                        .email("abs.junnior@hotmail.com")));
+	    }
+}
